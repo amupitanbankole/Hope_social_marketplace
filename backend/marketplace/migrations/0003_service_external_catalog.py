316c4127def916053e035cede23d176aead53df1
Bankole Amupitan <amupitanmichael@outlook.com>
2026-09-09 12:24:05 +0100

    chore: add Marketreum catalog migration placeholder

---
+++ backend/marketplace/migrations/0003_service_external_catalog.py
@@ -0,0 +1,9 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0002_alter_service_options_service_external_url_and_more"),
+    ]
+
+    operations = []
