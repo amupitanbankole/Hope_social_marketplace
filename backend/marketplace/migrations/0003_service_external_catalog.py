9bf4f418d06f9245bc86643e4b133db3e429da90
Bankole Amupitan <amupitanmichael@outlook.com>
2026-09-09 12:24:10 +0100

    cleanup: remove duplicate Marketreum migration placeholder

--- backend/marketplace/migrations/0003_service_external_catalog.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("marketplace", "0002_alter_service_options_service_external_url_and_more"),
-    ]
-
-    operations = []
